ffbbf4241131520d79321ee96f92dff93d5ee661
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-03-08 04:08:49 +0100

    initial config for appveyor testing in MS edge

---
+++ .config/postinstall.js
@@ -0,0 +1,24 @@
+/*
+	 THIS SCRIPT WILL DETECT IF THE OS IS WINDOWS AND IF YES THEN
+	 INSTALL THE NESSEARY NPM MODULES TO EXECUTE THE TEST SUITE
+	 IN IE11 AND EDGE
+*/
+
+if(process.platform === 'win32') {
+
+	console.log('Windows detected. Installing Microsoft Edge and IE11 test launchers...')
+
+	const exec = require('child_process').exec
+
+	exec('npm install karma-edge-launcher --production --save-dev', (err, stdout, stderr) => {
+
+		if(err) {
+			console.error(err)
+			return
+		}
+
+		console.log(stdout)
+
+	})
+
+}
